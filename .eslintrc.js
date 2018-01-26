1bb0fd85a5bde5fd3749c321c817adc7f0af2210
Nidhi Jagadeesha <nidhijune14@gmail.com>
2018-01-26 12:58:59 +0530

    fixed eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,11 +1,5 @@
 module.exports = {
-    "extends": "eslint:recommended",
-    "rules": {
-        "semi": ["error", "always"],
-        "quotes": ["error", "single"],
-        "no-console":"off",
-        "indent" : [2, 2, {"SwitchCase": 1}]
-    },
+    "extends": "airbnb",
     "env": { "es6": true,
     "jest":true,
     "node":true,
