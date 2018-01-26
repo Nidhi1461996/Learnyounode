0334f5a0f43309cb03263ee825f0039a19c11478
Nidhi Jagadeesha <nidhijune14@gmail.com>
2018-01-26 12:57:46 +0530

    ..

--- babySteps.test.js
+++ babySteps.test.js
@@ -1,24 +1,23 @@
 const babySteps = require('./babySteps');
 
 describe('Testing all possible valid inputs', () => {
-  
-  test('All positive numbers', () =>{
-    expect(babySteps(['1','2','3'])).toBe(6);
+  test('All positive numbers', () => {
+    expect(babySteps(['1', '2', '3'])).toBe(6);
   });
-  test('All negative numbers', () =>{
-    expect(babySteps(['-1','-2','-3'])).toBe(-6);
+  test('All negative numbers', () => {
+    expect(babySteps(['-1', '-2', '-3'])).toBe(-6);
   });
 
   test('With both positive and negative inputs', () => {
-    expect(babySteps(['1','2','-3'])).toBe(0);
+    expect(babySteps(['1', '2', '-3'])).toBe(0);
   });
 
   test('With all 0s', () => {
-    expect(babySteps(['0','0','0'])).toBe(0);
+    expect(babySteps(['0', '0', '0'])).toBe(0);
   });
 
   test('check if a number is returned', () => {
-    expect(typeof babySteps(['1','2','-3'])).toBe('number');
+    expect(typeof babySteps(['1', '2', '-3'])).toBe('number');
   });
 
   test('With no input', () => {
